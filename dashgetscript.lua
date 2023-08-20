0d955b1f9358292461aee4d7094c46534410aaa0
nuc2222 <122896690+nuc2222@users.noreply.github.com>
2023-08-20 09:37:54 +0300

    Update dashgetscript.lua
    nvm just use this script and coppy the main none load script to modify it

--- dashgetscript.lua
+++ dashgetscript.lua
@@ -1,7 +1,1 @@
-dashPower = 40 -- You can change this value to the desired amount of studs to move forward
-dashSpeed = 100 -- You can adjust this value to control how fast the character reaches the destination
-
-local function runRemoteScript(power, speed)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/nuc2222/dash/main/script.lua"))()(power, speed)
-end
-runRemoteScript(dashPower, dashSpeed)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/nuc2222/dash/main/script.lua"))()
