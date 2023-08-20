14da1b0d5a482c504267b5df22cc1d1f2d616858
nuc2222 <122896690+nuc2222@users.noreply.github.com>
2023-08-20 21:05:54 +0300

    Update scripts.lua

--- scripts.lua
+++ scripts.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/nuc2222/dash/main/script.lua"))()
+print('loadstring(game:HttpGet("https://raw.githubusercontent.com/nuc2222/dash/main/script.lua"))()')
