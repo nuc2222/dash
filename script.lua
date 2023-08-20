278b3eda5208ccd98fd097be1058b7dc97422265
nuc2222 <122896690+nuc2222@users.noreply.github.com>
2023-08-20 09:50:12 +0300

    Update script.lua
    added better cusom speed use /dp do edit dashpower and /ds to edit dashspeed
    examples:
    /dp 50
    /ds 100

--- script.lua
+++ script.lua
@@ -1,8 +1,12 @@
+local dashPower = 40 -- Default value
+local dashSpeed = 100 -- You can adjust this value to control how fast the character reaches the destination
+
 print("script executed!")
 print("dashspeed:")
 print(dashSpeed)
 print("dashpower:")
 print(dashPower)
+
 local player = game.Players.LocalPlayer
 local character = player.Character or player.CharacterAdded:Wait()
 local humanoid = character:WaitForChild("Humanoid")
@@ -44,8 +48,21 @@ local function dash()
     isDashing = false
 end
 
+local function onChat(message)
+    local cmd, value = message:match("/(%w+) (%d+)")
+    if cmd == "dp" and value then
+        dashPower = tonumber(value)
+        print("New dashPower set: " .. dashPower)
+    elseif cmd == "ds" and value then
+        dashSpeed = tonumber(value)
+        print("New dashSpeed set: " .. dashSpeed)
+    end
+end
+
 userInputService.InputBegan:Connect(function(input, gameProcessedEvent)
     if not gameProcessedEvent and input.KeyCode == Enum.KeyCode.Q then
         dash()
     end
 end)
+
+game:GetService("Players").LocalPlayer.Chatted:Connect(onChat)
